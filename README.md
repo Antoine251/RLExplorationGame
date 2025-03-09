e32b9a2ee816cb4d7c9fd0c88316f3261ff5d8a5
Antoine251 <antoine.laborde@inspire.ch>
2025-03-09 22:46:12 +0100

    Clean up and add demo

---
+++ README.md
@@ -0,0 +1,7 @@
+### Demo of the trained agent!
+
+https://github.com/user-attachments/assets/104eaec7-236b-4e42-9327-b8ab5e88c05e
+
+Two noticeable flaws of the agent:
+- It doesn't go around corners well.
+- It gets stuck in "local minima": a succession of left, right, left, right, left... actions.
--- train_network.py
+++ train_network.py
@@ -27,7 +27,10 @@ input_size = NUMBER_OF_RAYS * 3
 output_size = 3  # Actions: turn left, forward, turn right
 model = DQN(input_size, output_size)
 target_network = DQN(input_size, output_size)
-model.load_state_dict(torch.load("./models/trained_model_v1.pth"))
+initial_model = "./models/trained_model_v1.pth"
+if initial_model is not None:
+    model.load_state_dict(torch.load(initial_model))
+    target_network.load_state_dict(torch.load(initial_model))
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.MSELoss()
 
@@ -50,19 +53,15 @@ for episode in range(num_episodes):
                 act_index = random.randint(0, 2)
                 act = [0, 0, 0]
                 act[act_index] = 1
-                # act = [random.choice([0, 1]) for _ in range(output_size)]
-                # while act[0] == 1 and act[2] == 1:
-                #     act = [random.choice([0, 1]) for _ in range(output_size)]
                 action.append(act)
         else:
             for i in range(NBR_OF_FISHES):
                 with torch.no_grad():
                     q_out = model(state_tensor[i]).squeeze()
-                    # act = list((q_out > action_threshold).int()[0])
-                    max_index = torch.argmax(q_out)  # Gets the index of the max value
+                    max_index = torch.argmax(q_out)
 
                     # Create a one-hot encoded tensor
-                    act = torch.zeros_like(q_out)  # Create a tensor of zeros with the same shape
+                    act = torch.zeros_like(q_out)
                     act[max_index] = 1
                     action.append(list(act))
 
@@ -79,49 +78,35 @@ for episode in range(num_episodes):
                 replay_buffer.append((state[i], action[i], reward[i], next_state[i], done[i]))
 
         # Sample and train
-        if len(replay_buffer) > batch_size:
-            batch = random.sample(replay_buffer, batch_size)
+        # if len(replay_buffer) > batch_size:
+        #     batch = random.sample(replay_buffer, batch_size)
 
-            # Prepare batch data
-            states, actions, rewards, next_states, dones = zip(*batch)
-            states = np.array(states)
-            next_states = np.array(next_states)
-            states = torch.FloatTensor(states).reshape([batch_size, NUMBER_OF_RAYS * 3])
-            actions = torch.FloatTensor(actions)
-            rewards = torch.FloatTensor(rewards)
-            next_states = torch.FloatTensor(next_states).reshape([batch_size, NUMBER_OF_RAYS * 3])
-            dones = torch.FloatTensor(dones)
+        #     # Prepare batch data
+        #     states, actions, rewards, next_states, dones = zip(*batch)
+        #     states = np.array(states)
+        #     next_states = np.array(next_states)
+        #     states = torch.FloatTensor(states).reshape([batch_size, NUMBER_OF_RAYS * 3])
+        #     actions = torch.FloatTensor(actions)
+        #     rewards = torch.FloatTensor(rewards)
+        #     next_states = torch.FloatTensor(next_states).reshape([batch_size, NUMBER_OF_RAYS * 3])
+        #     dones = torch.FloatTensor(dones)
 
-            # print(f"States size: {states.size()}")
-            # print(f"Next states size: {next_states.size()}")
-            # print(f"Rewards size: {rewards.size()}")
-            # print(f"Actions size: {actions.size()}")
-            # print(f"Dones size: {dones.size()}")
-            # print(torch.sum(dones))
-            # print(f"Rewards reshaped: {rewards.unsqueeze(1).expand(-1, 3).size()}")
+        #     # Compute Q-values and select the one linked to the performed action
+        #     q_values = model(states)
+        #     q_values = (q_values * actions).sum(dim=1, keepdim=True).squeeze()
 
-            # Compute Q-values and select the one linked to the performed action
-            q_values = model(states)
-            q_values = (q_values * actions).sum(dim=1, keepdim=True).squeeze()
-            # print(f"Q-values size: {q_values.size()}")
+        #     with torch.no_grad():
+        #         next_q_values = target_network(next_states)
 
-            with torch.no_grad():
-                next_q_values = target_network(next_states)
+        #     next_q_values = next_q_values.max(1).values
 
-            # print(f"next Q-values size: {next_q_values.size()}")
-            # print(next_q_values[0:3])
-            next_q_values = next_q_values.max(1).values
-            # print(f"next Q-values size: {next_q_values.size()}")
-            # print(next_q_values[0:3])
+        #     targets = rewards + gamma * next_q_values
 
-            targets = rewards + gamma * next_q_values
-            # targets = targets.squeeze(1)
-
-            # Update the model
-            loss = criterion(q_values, targets)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        #     # Update the model
+        #     loss = criterion(q_values, targets)
+        #     optimizer.zero_grad()
+        #     loss.backward()
+        #     optimizer.step()
 
         # Update state
         state = next_state
@@ -140,7 +125,7 @@ for episode in range(num_episodes):
     print(f"Episode {episode + 1}: Total Reward: {total_reward}")
     print(f"New epsilon: {epsilon}")
 
-    if episode % 5 == 0:
-        model_path = f"./models/last_model.pth"
-        target_network.load_state_dict(model.state_dict())
-        torch.save(model.state_dict(), model_path)
+    # if episode % 5 == 0:
+    #     model_path = f"./models/last_model.pth"
+    #     target_network.load_state_dict(model.state_dict())
+    #     torch.save(model.state_dict(), model_path)
